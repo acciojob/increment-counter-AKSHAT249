d3f57870b766d0bc96048de45fcc63f4dca8a82b
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-16 15:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function increase(){
+  let temp  =parseInt(document.getElementById("counter").innerHTML);
+  document.getElementById("counter").innerHTML = temp+1;
+  alert(temp);
+
+}
